e0443707a49f6ca48ca582c2c9bb1e51c3423ded
Wilson Tomisin <tommywilson972@gmail.com>
2024-10-09 13:52:48 +0100

    added drop down

--- src/components/DropdownLinks.tsx
+++ src/components/DropdownLinks.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { AlignLeft } from 'lucide-react';
+import { Button } from './ui/button';
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
 
 function DropdownLinks() {
   return (
-    <div>
-      DropdownLinks
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden '>
+            <Button variant={'outline'} size={'icon'}>
+                <AlignLeft/>
+                <span className='sr-only'>Toggle links</span>
+            </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+       align='start'
+       sideOffset={25}
+       className='lg:hidden w-52'>
+            {links.map((link)=>{
+                return <DropdownMenuItem key={link.label}>
+                    <NavLink to={link.href} className={(({isActive})=>{
+                        return ` capitalize w-full ${ isActive ? ' text-primary': ''}`
+                    })}>
+                        {link.label}
+                    </NavLink>
+                </DropdownMenuItem>
+            })}
+
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
